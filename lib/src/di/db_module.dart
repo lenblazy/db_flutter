fea2ec1f1a234520f20d62773aa24085f933f04b
Lennox Mwabonje <lenblazy@gmail.com>
2026-01-05 10:17:39 +0300

    Add storage dependency for secured sqllite

--- lib/src/di/db_module.dart
+++ lib/src/di/db_module.dart
@@ -1,6 +1,7 @@
 import "package:injectable/injectable.dart";
 import "package:path/path.dart";
 import "package:sqflite_common/sqflite.dart";
+import "package:storage_flutter/storage.dart";
 
 import "../../db.dart";
 import "../db/db_constants.dart";
@@ -10,7 +11,7 @@ import "../db/sqlite/sqlite_db_service.dart";
 @module
 abstract class DbModule {
   @lazySingleton
-  Future<DbService> dbService() async {
+  Future<DbService> dbService(Storage storage) async {
     final String dbPath = await getDatabasesPath();
     final String path = join(dbPath, DbConstants.dbName);
     final Database db = await openDatabase(
